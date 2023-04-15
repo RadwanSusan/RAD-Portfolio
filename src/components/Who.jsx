6a90d2503d9a0973e74c862a01867a4581466998
radwan susan <radwansusan90@gmail.com>
2023-04-16 01:57:02 +0300

    Update Who.jsx

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -57,11 +57,12 @@ function Who() {
 					</Description>
 					<form
 						name='contact'
-						netlify
+						method='POST'
+						data-netlify='true'
 					>
 						<p>
 							<label>
-								Name{' '}
+								Your Name:{' '}
 								<input
 									type='text'
 									name='name'
@@ -70,11 +71,28 @@ function Who() {
 						</p>
 						<p>
 							<label>
-								Email{' '}
+								Your Email:{' '}
 								<input
 									type='email'
 									name='email'
 								/>
+							</label>
+						</p>
+						<p>
+							<label>
+								Your Role:{' '}
+								<select
+									name='role[]'
+									multiple
+								>
+									<option value='leader'>Leader</option>
+									<option value='follower'>Follower</option>
+								</select>
+							</label>
+						</p>
+						<p>
+							<label>
+								Message: <textarea name='message'></textarea>
 							</label>
 						</p>
 						<p>
