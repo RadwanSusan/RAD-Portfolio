694b5fd3d1f63b4cf2c19c6a8183892397cb38f5
radwan susan <radwansusan90@gmail.com>
2023-05-31 21:18:50 +0400

    Update vite Plugins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import webfontDownload from 'vite-plugin-webfont-dl';
+import imagemin from 'unplugin-imagemin/vite';
+import banner from 'vite-plugin-banner';
+import progress from 'vite-plugin-progress';
 
 export default defineConfig({
-	plugins: [react(), webfontDownload()],
+	plugins: [
+		react(),
+		webfontDownload(),
+		progress(),
+		imagemin({
+			mode: 'sharp',
+			compress: {
+				jpg: {
+					quality: 70,
+				},
+				jpeg: {
+					quality: 70,
+				},
+				png: {
+					quality: 70,
+				},
+				webp: {
+					quality: 70,
+				},
+			},
+			conversion: [{ from: 'jpeg', to: 'webp' }],
+		}),
+		banner(`
+    ██   ██         ███████   ██      ██ ████████   ██    ██   ███████   ██     ██
+    ░██  ░██        ██░░░░░██ ░██     ░██░██░░░░░   ░░██  ██   ██░░░░░██ ░██    ░██
+    ░██  ░██       ██     ░░██░██     ░██░██         ░░████   ██     ░░██░██    ░██
+    ░██  ░██      ░██      ░██░░██    ██ ░███████     ░░██   ░██      ░██░██    ░██
+    ░██  ░██      ░██      ░██ ░░██  ██  ░██░░░░       ░██   ░██      ░██░██    ░██
+    ░██  ░██      ░░██     ██   ░░████   ░██           ░██   ░░██     ██ ░██    ░██
+    ░██  ░████████ ░░███████     ░░██    ░████████     ░██    ░░███████  ░░███████
+    ░░   ░░░░░░░░   ░░░░░░░       ░░     ░░░░░░░░      ░░      ░░░░░░░    ░░░░░░░
+    `),
+	],
 });
